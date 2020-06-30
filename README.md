6b7ab22e69bf6e9b6402b3364ffecec285a3031e
Paweł Szymczak <mangoman@wp.pl>
2020-06-30 11:24:52 +0200

    add project currency

---
+++ README.md
@@ -0,0 +1,13 @@
+﻿# This is Currency Converter
+
+
+
+
+Hello!
+This first Currency Converter in my profile. Maybe next time more projects of currency converter :).
+Demo:
+
+
+
+Thank you and best regards,
+PSz
---
+++ js/script.js
@@ -0,0 +1,50 @@
+{
+    const welcomeFunction = () => {
+        console.log("Hello Client")
+    };
+
+    welcomeFunction();
+
+    const calculateResult = (amount, currency) => {
+
+        const rateEUR = 4.45;
+        const rateUSD = 3.95;
+        const rateGBP = 4.94;
+        const ratePLN = 1;
+
+        switch (currency) {
+            case "EUR":
+                return amount / rateEUR;
+
+            case "USD":
+                return amount / rateUSD;
+
+            case "GBP":
+                return amount / rateGBP;
+
+            case "PLN":
+                return amount / ratePLN;
+        };
+    };
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+
+        formElement.addEventListener("submit", (event) => {
+            event.preventDefault();
+
+            const amountElement = document.querySelector(".js-amount");
+            const currencyElement = document.querySelector(".js-currency");
+            const exchangeElement = document.querySelector(".js-exchangeValue");
+            const amount = +amountElement.value;
+            const currency = currencyElement.value;
+
+            const result = calculateResult(amount, currency);
+
+            exchangeElement.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency} </strong>`;
+
+        });
+    };
+
+    init();
+}
